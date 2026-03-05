dd631a415050b5d89702cff47976171590b94e73
Henry Alfredo Garcia Palacio <henrygarcia-15@hotmail.com>
2026-03-05 16:57:56 -0500

    se realizan cambios para poder subir el sitio

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/rendicion-de-cuentas-25/",
   server: {
     host: "::",
     port: 8080,
